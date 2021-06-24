1812d8430ca4b533f7155cb7bfa6401641542d37
MGrgr <grkapran@gmail.com>
2021-06-24 13:33:43 +0300

    language change start

--- layouts/default.js
+++ layouts/default.js
@@ -1,7 +1,21 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from 'next/router'
+import { useEffect } from "react";
 
-const DefaultLayout = (props) => (
+const DefaultLayout = (props) => {
+
+  const router = useRouter();
+  const handleLanguageChange = (event) => {
+    router.push(router.route, null ,{
+      locale: event.currentTarget.value
+    })
+  }
+
+  useEffect(() => {
+    console.log('language changed')
+  }, [router.locale])
+  return (
   <>
     <Head>
       <title>Default</title>
@@ -21,15 +35,15 @@ const DefaultLayout = (props) => (
         <Link href="/admin" tag="a">
           Admin Panel
         </Link>
-        <select className="changeLanguage">
+        <select className="changeLanguage" value={router.locale} onChange={handleLanguageChange}>
           <option value="en">EN</option>
-          <option value="FR">FR</option>
-          <option value="RO">RO</option>
+          <option value="fr">FR</option>
+          <option value="ro">RO</option>
         </select>
       </div>
       <div>{props.children}</div>
     </div>
   </>
-);
+)};
 
 export default DefaultLayout;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Home from "../components/home/home";
 import { API } from "../api/api"
+import { useRouter } from "next/router";
 
 const IndexPage = ({ data }) => {
   return (
@@ -7,7 +8,9 @@ const IndexPage = ({ data }) => {
   )
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (context) => {
+
+  console.log(context.locale);
 
   const content = await API.getContent();
   return {
